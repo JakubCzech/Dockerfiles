fa869dc4c86684f9f00401b30a67aa6abfcbbc6b
JakubCzech <czechjakub@icloud.com>
2022-12-07 13:30:22 +0100

    Update bash files

--- create_container.sh
+++ create_container.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-docker build -t tello:foxy .
 xhost +
 docker run -it --name=$1 \
 --ulimit memlock=-1 \
 --privileged --env="QT_X11_NO_MITSHM=1" \
--v /home/$SUDO_USER/Shared:/root/Shared:rw \
 --device=/dev/dri:/dev/dri --device=/dev/video0 \
 --device=/dev/shm:/dev/shm \
 --env="DISPLAY=$DISPLAY" --network=host \
